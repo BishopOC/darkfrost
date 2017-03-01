05f0b7cb8a9dc22972ecda145d6f2ea86be41fb4
Bishop O'Connell <bishopoconnell1@gmail.com>
2017-03-01 18:30:21 -0500

    Made it pretty

--- public/images/Dark Frost_files/app.js
+++ public/images/Dark Frost_files/app.js
@@ -1,25 +1,21 @@
 var currentlyWidget = new Vue({
   el: '#currently',
   data: {
-    time: 0,
-    summary: ' ',
-    icon: ' ',
-    apparentTemperature: 0,
-    precipProbability : 0,
-    humidity: 0,
-    location:' ',
+    time: 1000000,
+    summary: 'Partly Cloudy',
+    icon: 'partly-cloudy',
+    apparentTemperature: 67.4,
+    precipProbability : 0.30,
+    humidity: 0.61,
+    location:[],
   },
   methods: {
     iconUrl: function(iconString){
       return `/images/${iconString}.png`;
-    },
-    timeFilter: function(time){
-      return moment(time).format('MMMM Do YYYY, h:mm:ss a');
-    },
-
+    }
   },
   created: function(){
-    axios.get('/weather/29.67,-82.36')
+    axios.get('/weather/29.7,-82.4')
          .then(function(response){
            currentlyWidget.time = response.data.currently.time;
            currentlyWidget.summary = response.data.currently.summary;
@@ -27,28 +23,28 @@ var currentlyWidget = new Vue({
            currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
            currentlyWidget.precipProbability = response.data.currently.precipProbability;
            currentlyWidget.humidity = response.data.currently.humidity;
-           console.log(response.data);
          })
          .catch(function(err){
            console.log(err);
          });
-    axios.get('/location/29.67,-82.36')
+    axios.get('/location/29.7,-82.4')
          .then(function(response){
-           currentlyWidget.location = response.data.results[2].formatted_address;
+           currentlyWidget.location = response.data.results[1].formatted_address;
           //  console.log(currentlyWidget.location);
-           console.log(response.data);
+          //  console.log(response.data);
          });
-
   },
-
-
+  interval: function(){
+    var timer = setInterval(created.axios.get(), 1000);
+    console.log(timer);
+  }
 });
 
 var dailyWidget = new Vue({
   el: '#daily',
   data: {
-    icon: ' ',
-    summary: ' '
+    icon: 'partly-cloudy',
+    summary: 'partly cloudy'
   },
   methods: {
     iconUrl: function(iconString){
